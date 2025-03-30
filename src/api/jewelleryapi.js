e44d45e46105858c493d71e80192bcaa1c36fdb3
Rahul Tak <rahultak955@gmail.com>
2025-03-30 22:04:53 +0530

    add jewellery

--- src/api/jewelleryapi.js
+++ src/api/jewelleryapi.js
@@ -1,6 +1,19 @@
 export const JewelleryData = async()=>{
-    const response = await fetch("http:localhost:8000//api/jewellery/fetchalljewellery",{method:"GET",
+    const response = await fetch("http://localhost:8000/jewellery/",{method:"GET",
 
     });
     return await response.json();
+}
+
+
+export const CreateJewellery = async(formData)=>
+{
+    console.log(formData)
+    const response = await fetch("http://localhost:8000/jewellery/addjewellery",{method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+    return await response.json()
 }
--- src/pages/Allproduct.js
+++ src/pages/Allproduct.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { JewelleryData } from '../api/jewelleryapi';
 
 
-const Allproduct = ({ data = [] }) => {
+const Allproduct = () => {
+const [data, setData]= useState([])
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const result = await JewelleryData();
+      console.log("fetch jewellery", result)
+
+      setData(result.jewellery || []);
+    }
+
+    fetchData();
+  },[])
   return (
     <div>
       {/* <section className="shop_section layout_padding-t">
@@ -11,26 +24,26 @@ const Allproduct = ({ data = [] }) => {
           </div>
           <div className="row">
             {data.length > 0 ? (
-              data.map((product) => (
+              data.map((jewellery) => (
                 <div
                   className="col-sm-6 col-md-4 col-lg-3"
-                  key={product.id || product._id} 
+                  key={jewellery.id || jewellery._id} 
                 >
                   <div className="box">
-                    <a href={`/single?id=${product.id}`} className="text-decorastion">
+                    <a href={`/single?id=${jewellery.id}`} className="text-decorastion">
                       <div className="img-box">
                         <img
-                          src={product.image || "assets/images/p8.png"} 
-                          alt={product.jewellery_name}
+                          src={jewellery.image || "assets/images/p8.png"} 
+                          alt={jewellery.fullName}
                         />
                       </div>
                       <div className="detail-box">
-                        <h6>{product.jewellery_name}</h6>
+                        <h6>{jewellery.jewellery_name}</h6>
                         <h6>
-                          Price <span>${product.price || "N/A"}</span>
+                          Price <span>${jewellery.price || "N/A"}</span>
                         </h6>
                       </div>
-                      {product.isNew && (
+                      {jewellery.isNew && (
                         <div className="new">
                           <span>New</span>
                         </div>
@@ -40,7 +53,7 @@ const Allproduct = ({ data = [] }) => {
                 </div>
               ))
             ) : (
-              <p>No products available</p>
+              <p>No jewellerys available</p>
             )}
           </div>
           <div className="btn-box">
@@ -48,6 +61,9 @@ const Allproduct = ({ data = [] }) => {
           </div>
         </div>
       </section> */}
+
+
+
         <section className="shop_section layout_padding">
     <div className="container">
       <div className="heading_container heading_center">
@@ -56,6 +72,10 @@ const Allproduct = ({ data = [] }) => {
         </h2>
       </div>
       <div className="row">
+        {Array.isArray(data)&& data.length>0 ?(
+          data.map((jewellery, id)=>(
+
+          
         <div className="col-sm-6 col-md-4 col-lg-3">
           <div className="box">
             <a href="" className='text-decorastion'>
@@ -63,13 +83,13 @@ const Allproduct = ({ data = [] }) => {
                 <img src="assets/images/p1.png" alt=""/>
               </div>
               <div className="detail-box">
-                <h6>
-                  Necklace
+                <h6 key={id}>
+                  {jewellery.jewelleryname}
                 </h6>
                 <h6>
                   Price
                   <span>
-                    $200
+                    ${jewellery.price}
                   </span>
                 </h6>
               </div>
@@ -81,181 +101,12 @@ const Allproduct = ({ data = [] }) => {
             </a>
           </div>
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p2.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Necklace
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $300
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p3.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Necklace
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $110
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p4.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Ring
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $45
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p5.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Ring
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $95
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p6.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Earrings
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $70
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p7.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Earrings
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $400
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="box">
-            <a href="" className='text-decorastion'>
-              <div className="img-box">
-                <img src="assets/images/p8.png" alt=""/>
-              </div>
-              <div className="detail-box">
-                <h6>
-                  Necklace
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $450
-                  </span>
-                </h6>
-              </div>
-              <div className="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
+
+))
+):(
+  <p>no data found</p>
+)}
+  
       </div>
       <div className="btn-box ">
         <a href="/allproduct" className='text-decorastion'>
